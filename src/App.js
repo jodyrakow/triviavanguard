f2b5373a02fc454450f9565a6b752f11a0eb94bd
Jody Rakow <jodyrakow@Jodys-MacBook-Pro.local>
2025-12-17 19:00:53 -0600

    Wed again

--- src/App.js
+++ src/App.js
@@ -982,17 +982,6 @@ export default function App() {
             );
           }
 
-          // Set location if provided
-
-          if (
-            config.location &&
-            (!currentHostInfo.location ||
-              config.location !== currentHostInfo.location)
-          ) {
-            updatedHostInfo.location = config.location;
-            hasChanges = true;
-          }
-
           // Set pool contribution if provided
           if (typeof config.poolContribution === "number") {
             console.log(
@@ -1048,6 +1037,17 @@ export default function App() {
           if (hasChanges) {
             patchShared({ hostInfo: updatedHostInfo });
           }
+        }
+
+        // Set location if provided
+
+        if (
+          config.location &&
+          (!currentHostInfo.location ||
+            config.location !== currentHostInfo.location)
+        ) {
+          updatedHostInfo.location = config.location;
+          hasChanges = true;
         }
 
         // set default round if needed
